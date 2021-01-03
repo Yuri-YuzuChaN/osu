99d8126c8475ef29481798f09a71b4b44a30ff7e
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-01-03 10:59:36 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,3 @@
-from hoshino import util
-import requests
-import json
-
 def get_api():
-    jsonpath = './hoshino/modules/osu/'
-    with open(f'{jsonpath}config.json', encoding='utf-8') as d:
-        jsondata = json.load(d)
-        api = jsondata['apikey']
-        return api
+    api = ''
+    return api
