e0e81632839cd6d8dddb81f590d90021b760d3d4
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-01-06 23:31:45 +0800

    新增异步

--- api.py
+++ api.py
@@ -1,3 +1,15 @@
+
+
 def get_api():
     api = ''
     return api
+
+async def osuapi(url):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as re:
+                info = await re.json()
+        return info
+    except:
+        msg = 'API请求失败，请联系管理员'
+        return msg
