fca541b887315f225a56e5d99222d37be522bc5f
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-01-08 13:42:26 +0800

    修改获取文件名

--- osu_file.py
+++ osu_file.py
@@ -4,10 +4,10 @@ from contextlib import closing
 import hoshino
 import os
 import re
-import json
 import zipfile
 import aiohttp
 from PIL import Image
+from urllib import parse
 
 osupath = os.path.dirname(__file__)
 osufile = f'{osupath}/OsuFile/'
@@ -15,45 +15,48 @@ mapfile = f'{osufile}/map/'
 usericon = f'{osufile}/user_icon/'
 
 async def Download(mapid):
-    filename = f'{mapid}.osz'
-    filepath = f'{mapfile}{filename}'
     # 判断是否存在该文件
-    if os.path.exists(filepath[:-4]):
-        return filepath[:-4]
-    else:
-        url = f'https://txy1.sayobot.cn/beatmaps/download/full/{mapid}'
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, allow_redirects = False) as re:
-                    sayo = re.headers['Location']
-        except:
-            print('请求失败或超时')
-            return
-        await get_osz(filepath, sayo)
-        # 解压下载的osz文件
-        myzip = zipfile.ZipFile(filepath)
-        mystr = myzip.filename.split(".")
-        myzip.extractall(mystr[0])
-        myzip.close()
-        end = ['mp3','wav','mp4','avi','mov','ogg','osb']
-        # 删除其余不需要的文件
-        for root, dirs, files in os.walk(filepath[:-4], topdown=False):
-            for name in files:
-                for i in end:
-                    if name.endswith(i):
-                        os.remove(os.path.join(root, name))
+    for file in os.listdir(mapfile):
+        if mapid in file:
+            return file
+        else:
+            url = f'https://txy1.sayobot.cn/beatmaps/download/full/{mapid}'
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url, allow_redirects = False) as re:
+                        sayo = re.headers['Location']
+            except:
+                print('请求失败或超时')
+                return
+            filename = await get_osz(sayo)
+            filepath = mapfile + filename
+            # 解压下载的osz文件
+            myzip = zipfile.ZipFile(filepath)
+            mystr = myzip.filename.split(".")
+            myzip.extractall(mystr[0])
+            myzip.close()
+            end = ['mp3','wav','mp4','avi','mov','ogg','osb']
+            # 删除其余不需要的文件
+            for root, dirs, files in os.walk(filepath[:-4], topdown=False):
+                for name in files:
+                    for i in end:
+                        if name.endswith(i):
+                            os.remove(os.path.join(root, name))
 
-        # 删除下载osz文件
-        os.remove(filepath)
-        return filepath[:-4]
+            # 删除下载osz文件
+            os.remove(filepath)
+            return filepath[:-4]
         
-async def get_osz(filepath, sayo):
+async def get_osz(sayo):
     try:
         print('开始下载')
         async with aiohttp.ClientSession() as session:
             async with session.get(sayo) as req:
+                title = req.headers['Content-Disposition'].split('"')
+                filename = parse.unquote(title[1])
                 chunk = await req.read()
-                open(filepath, 'wb').write(chunk)
+                open(f'{mapfile}{filename}', 'wb').write(chunk)
+        return filename
     except:
         print('地图下载失败')
         return
