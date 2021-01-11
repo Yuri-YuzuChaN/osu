24e24389258dcea4542900cad37cf025a7d109fd
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-01-11 19:21:01 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,8 @@
 import os
-
 import aiohttp
 import hoshino
 from hoshino import Service, util
 from hoshino.typing import MessageSegment, NoticeSession, CQEvent
-import requests
-import json
 
 from .osusql import mysql
 from .api import get_api, osuapi
@@ -126,49 +123,49 @@ async def score(bot, ev:CQEvent):
     if '' in msg:
         msg.remove('')
     sql = f'select osuid,osumod,osuname from userinfo where qqid = {qqid}'
-    if msg:
-        if ':' in msg:
-            l_num = len(msg) - 2
-            if l_num != 0:
-                if msg[l_num].isdigit():
-                    mapid = msg[l_num]
-                    osuid = ' '.join(msg[:l_num])
-                    osumod = msg[l_num + 1][-1:]
-                else:
-                    await bot.finish(ev, '请输入正确的地图ID！')
-            elif msg[l_num].isdigit():
-                mapid = msg[l_num]
-                osumod = msg[l_num + 1][-1:]
-                result = mysql(sql)
-                if result:
-                    for i in result:
-                        osuid = i[2]
-                else:
-                    await bot.finish(ev, '该账号尚未绑定，请输入 bind 用户名 绑定账号')
-            else:
-                await bot.finish(ev, '请输入正确的地图ID！')
-        else:
-            l_num = len(msg) - 1
-            if l_num != 0:
-                if msg[l_num].isdigit():
-                    mapid = msg[l_num]
-                    osuid = ' '.join(msg[:l_num])
-                    osumod = 0
-                else:
-                    await bot.finish(ev, '请输入正确的地图ID！')
-            elif msg[l_num].isdigit():
-                mapid = msg[l_num]
-                result = mysql(sql)
-                if result:
-                    for i in result:
-                        osuid = i[2]
-                        osumod = i[1]
-                else:
-                    await bot.finish(ev, '该账号尚未绑定，请输入 bind 用户名 绑定账号')
-            else:
-                await bot.finish(ev, '请输入正确的地图ID！')
-    else:
+    if not msg:
         await bot.finish(ev, '请输入正确的地图ID！')
+    if ':' in msg:
+        l_num = len(msg) - 2
+        if l_num != 0:
+            if msg[l_num].isdigit():
+                mapid = msg[l_num]
+                osuid = ' '.join(msg[:l_num])
+                osumod = msg[l_num + 1][-1:]
+            else:
+                await bot.finish(ev, '请输入正确的地图ID！')
+        elif msg[l_num].isdigit():
+            mapid = msg[l_num]
+            osumod = msg[l_num + 1][-1:]
+            result = mysql(sql)
+            if result:
+                for i in result:
+                    osuid = i[2]
+            else:
+                await bot.finish(ev, '该账号尚未绑定，请输入 bind 用户名 绑定账号')
+        else:
+            await bot.finish(ev, '请输入正确的地图ID！')
+    else:
+        l_num = len(msg) - 1
+        if l_num != 0:
+            if msg[l_num].isdigit():
+                mapid = msg[l_num]
+                osuid = ' '.join(msg[:l_num])
+                osumod = 0
+            else:
+                await bot.finish(ev, '请输入正确的地图ID！')
+        elif msg[l_num].isdigit():
+            mapid = msg[l_num]
+            result = mysql(sql)
+            if result:
+                for i in result:
+                    osuid = i[2]
+                    osumod = i[1]
+            else:
+                await bot.finish(ev, '该账号尚未绑定，请输入 bind 用户名 绑定账号')
+        else:
+            await bot.finish(ev, '请输入正确的地图ID！')
+
     url = f'{osu_api}get_scores?k={key}&b={mapid}&u={osuid}&m={osumod}'
     msg_ = await draw_score(url, osuid, osumod, mapid)
     if msg_:
@@ -184,33 +181,28 @@ async def score(bot, ev:CQEvent):
 async def bind(bot, ev:CQEvent):
     qqid = ev.user_id
     uid = ev.message.extract_plain_text()
-    if uid:
-        sql = f'select * from userinfo where qqid = {qqid}'
-        result = mysql(sql)
-        if not result:
-            url = f'{osu_api}get_user?k={key}&u={uid}'
-            info = await osuapi(url)
-            if 'API' in info:
-                msg = info
-            elif info:
-                for i in info:
-                    osuid = i['user_id']
-                    osuname = i['username']
-                try:
-                    sql = f'insert into userinfo values (NULL, {qqid}, {osuid}, "{osuname}", 0)'
-                    result = mysql(sql)
-                    if result:
-                        msg = f'用户 {osuname} 已成功绑定QQ {qqid}'
-                    else:
-                        msg = '绑定失败！'
-                except:
-                    msg = '未知错误，绑定失败！'
-            else:
-                msg = '未查询到该用户！'
-        else:
-            msg = '您已绑定，如需要解绑请输入unbind，改绑请输入update osuid 用户名'
+    if not uid:
+        await bot.finish(ev, '请输入您的 osuid')
+    sql = f'select * from userinfo where qqid = {qqid}'
+    result = mysql(sql)
+    if result:
+        await bot.finish(ev, '您已绑定，如需要解绑请输入unbind，改绑请输入update osuid 用户名')
+    url = f'{osu_api}get_user?k={key}&u={uid}'
+    info = await osuapi(url)
+    if 'API' in info:
+        msg = info
+    elif not info:
+        msg = '未查询到该用户！'
     else:
-        msg = '请输入您的 osuid'
+        for i in info:
+            osuid = i['user_id']
+            osuname = i['username']
+            sql = f'insert into userinfo values (NULL, {qqid}, {osuid}, "{osuname}", 0)'
+            result = mysql(sql)
+            if result:
+                msg = f'用户 {osuname} 已成功绑定QQ {qqid}'
+            else:
+                msg = '绑定失败！'
     await bot.send(ev, msg)
 
 @sv.on_prefix('unbind')
@@ -231,71 +223,74 @@ async def unbind(bot, ev:CQEvent):
 @sv.on_prefix('update')
 async def update(bot, ev:CQEvent):
     qqid = ev.user_id
-    msg = ev.message.extract_plain_text().strip().split(' ')
-    if '' in msg:
-        msg.remove('')
+    num = ev.message.extract_plain_text().strip().split(' ')
+    if '' in num:
+        num.remove('')
     sql = f'select * from userinfo where qqid = {qqid}'
     result = mysql(sql)
-    if result:
-        if msg:
-            if msg[0] == 'icon':
-                select_id = f'select osuid from userinfo where qqid = {qqid}'
-                result = mysql(select_id)
-                if result:
-                    for i in result:
-                        osuid = i[0]
-                    get_user_icon(osuid)
-                    get_user_header(osuid)
-                    await bot.send(ev, '头像更新完成')
-                else:
-                    await bot.send(ev, '该用户未绑定，无法更新头像！')
-            elif msg[0] == 'osuid':
-                try:
-                    osuid = msg[1]
-                except:
-                    await bot.finish(ev, '请输入更改的用户名！')
-                info = await select_info(osuid)
-                if info:
-                    for i in info:
-                        osunum = i['user_id']
-                        osuname = i['username']
-                    update_id = f'update userinfo set osuid = {osunum}, osuname = "{osuname}" where qqid = {qqid}'
-                    result = mysql(update_id)
-                    if result:
-                        await bot.send(ev, f'已将QQ {qqid} 绑定的osu用户名更改为 {osuname}')
-                    else:
-                        await bot.send(ev, '数据库错误')
-                else:
-                    await bot.send(ev, '未查询到该用户，无法更改用户名！')
-            else:
-                await bot.send(ev, '参数错误，请输入正确的参数！')
+    osuid = ''
+    if not result:
+        msg = '该账号尚未绑定，请输入 bind 用户名 绑定账号'
+    elif not num:
+        msg = '请输入需要更新内容的参数！'
+    elif num[0] == 'icon':
+        select_id = f'select osuid from userinfo where qqid = {qqid}'
+        result = mysql(select_id)
+        if result:
+            for i in result:
+                osuid = i[0]
+            get_user_icon(osuid)
+            get_user_header(osuid)
+            msg = '头像更新完成'
         else:
-            await bot.send(ev, '请输入需要更新内容的参数！')
+            msg = '该用户未绑定，无法更新头像！'
+    elif num[0] == 'osuid':
+        try:
+            osuid = num[1]
+        except:
+            msg = '请输入更改的用户名！'
+        info = await select_info(osuid)
+        if info:
+            for i in info:
+                osunum = i['user_id']
+                osuname = i['username']
+            update_id = f'update userinfo set osuid = {osunum}, osuname = "{osuname}" where qqid = {qqid}'
+            result = mysql(update_id)
+            if result:
+                msg = f'已将QQ {qqid} 绑定的osu用户名更改为 {osuname}'
+            else:
+                msg = '数据库错误'
+        else:
+            msg = '未查询到该用户，无法更改用户名！'
     else:
-        await bot.send(ev, '该账号尚未绑定，请输入 bind 用户名 绑定账号')
+        msg = '参数错误，请输入正确的参数！'
+    await bot.send(ev, msg)
 
 @sv.on_prefix('mode')
 async def mode(bot, ev:CQEvent):
     qqid = ev.user_id
     num = ev.message.extract_plain_text()
+    try:
+        num = int(num)
+    except:
+        await bot.finish(ev, '请输入正确的模式 0-3')
     sql = f'select * from userinfo where qqid = {qqid}'
     result = mysql(sql)
-    if result:
-        if num:
-            if num != 0 or 1 or 2 or 3:
-                mid = mod[f'{num}']
-                sql = f'update userinfo set osumod = {num} where qqid = {qqid}'
-                result = mysql(sql)
-                if result:
-                    await bot.send(ev, f'已将默认模式更改为 {mid}')
-                else:
-                    await bot.send(ev, '数据库错误')
-            else:
-                await bot.send(ev, '请输入正确的模式 0-3')
+    if not result:
+        msg = '该账号尚未绑定，请输入 bind 用户名 绑定账号'
+    elif not num:
+        msg = '请输入正确的模式 0-3'
+    elif num == 0 or num == 1 or num == 2 or num == 3:
+        mid = mod[f'{num}']
+        sql = f'update userinfo set osumod = {num} where qqid = {qqid}'
+        result = mysql(sql)
+        if result:
+            msg = f'已将默认模式更改为 {mid}'
         else:
-            await bot.send(ev, '请输入正确的模式 0-3')
+            msg = '数据库错误'
     else:
-        await bot.send(ev, '该账号尚未绑定，请输入 bind 用户名 绑定账号')
+        msg = '请输入正确的模式 0-3'
+    await bot.send(ev, msg)
 
 @sv.on_fullmatch('osuhelp')
 async def help(bot, ev:CQEvent):
