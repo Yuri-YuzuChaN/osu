9ae517ea105a03914e53f3ce0f1fc8f052f7157f
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-02-12 12:36:35 +0800

    Update osu_pp.py

--- osu_pp.py
+++ osu_pp.py
@@ -1,64 +1,63 @@
 from .oppai.oppai import *
+import pyttanko as osu
 
-# 计算95-100acc的pp
 def calc_acc_pp(osufile, mods_num):
-    pp = []
-    
-    ez = ezpp_new()
-    ezpp_set_autocalc(ez, 1)
-    ezpp_dup(ez, osufile)
-    ezpp_set_mods(ez, mods_num)
+    acc_pp = []
+    p = osu.parser()
+    with open(osufile, 'r', encoding='utf-8') as f:
+        bmap = p.map(f)
+
+    stars = osu.diff_calc().calc(bmap, mods_num)
     for acc in range(95, 101):
-        ezpp_set_accuracy_percent(ez, acc)
-        pp.append(int(ezpp_pp(ez)))
-    ezpp_free(ez)
+        c300, c100, c50 = osu.acc_round(acc, len(bmap.hitobjects), 0)
 
-    return pp
+        pp, _, _, _, _ = osu.ppv2(
+            stars.aim, stars.speed, mods=mods_num,
+            n300=c300, n100=c100, n50=c50, bmap=bmap
+        )
 
-# 计算游玩pp
-def calc_pp(osufile, mods_num, maxcb, c50, c100, c300, miss):
-    # cb总和
-    setcb = c50 + c100 + c300 + miss
-
-    ez = ezpp_new()
-    ezpp_set_autocalc(ez, 1)
-    ezpp_dup(ez, osufile)
-
-    # 输入mods
-    ezpp_set_mods(ez, mods_num)
-    # 计算开始计算pp前的难度
-    map_stars = ezpp_stars(ez)
-    # 输入最大连击cb数
-    ezpp_set_combo(ez, maxcb)
-    # 输入cb包括miss总和 
-    ezpp_set_end(ez, setcb)
-    # 输入100和50数量 计算acc
-    ezpp_set_accuracy(ez, c100, c50)
-    # 输入miss数量
-    ezpp_set_nmiss(ez, miss)
+        acc_pp.append(int(pp))
+        
+    return acc_pp
     
-    cs = ezpp_cs(ez)
-    ar = ezpp_ar(ez)
-    od = ezpp_od(ez)
-    hp = ezpp_hp(ez)
-    play_pp = round(ezpp_pp(ez), 2)
-    aim_pp = int(round(ezpp_aim_pp(ez), 2))
-    speed_pp = int(round(ezpp_speed_pp(ez), 2))
-    acc_pp = int(round(ezpp_acc_pp(ez), 2))
-    acc = round(ezpp_accuracy_percent(ez), 2)
+def calc_pp(osufile, mods_num, maxcb, c50, c100, c300, miss):
+    p = osu.parser()
+    with open(osufile, 'r', encoding='utf-8') as f:
+        bmap = p.map(f)
 
-    return map_stars, cs, ar, od, hp, play_pp, aim_pp, speed_pp, acc_pp, acc
+    stars = osu.diff_calc().calc(bmap, mods_num)
+    map_stars = stars.total
 
+    pp, aim, speed, acc, accuracy = osu.ppv2(
+        stars.aim, stars.speed, mods=mods_num,
+        combo=maxcb, n300=c300, n100=c100, n50=c50, nmiss=miss, bmap=bmap
+    )
+
+    cs = bmap.cs
+    ar = bmap.ar
+    od = bmap.od
+    hp = bmap.hp
+    play_pp = round(pp, 2)
+    aim_pp = int(round(aim, 2))
+    speed_pp = int(round(speed, 2))
+    acc_pp = int(round(acc, 2))
+    accuracy = round(accuracy, 2)
+
+    return map_stars, cs, ar, od, hp, play_pp, aim_pp, speed_pp, acc_pp, accuracy
+    
 def calc_if(osufile, mods_num, c50, c100, mapcb):
-    ez = ezpp_new()
-    ezpp_set_autocalc(ez, 1)
-    ezpp_dup(ez, osufile)
-    ezpp_set_mods(ez, mods_num)
-    ezpp_set_combo(ez, mapcb)
-    ezpp_set_accuracy(ez, c100, c50)
-    if_pp = ezpp_pp(ez)
-    ezpp_free(ez)
-    return if_pp
+    p = osu.parser()
+    with open(osufile, 'r', encoding='utf-8') as f:
+        bmap = p.map(f)
+
+    stars = osu.diff_calc().calc(bmap, mods_num)
+
+    pp = osu.ppv2(
+        stars.aim, stars.speed, mods=mods_num,
+        n100=c100, n50=c50, nmiss=0, max_combo=mapcb, bmap=bmap
+    )
+
+    return pp[0]
 
 def calc_time(osufile, c50, c100, c300, miss):
     setcb = c50 + c100 + c300 + miss
